7bb66ed7fae2e2167478d27a276f7efe1e028661
IshanGrover2004 <groverishan2004@gmail.com>
2023-11-05 18:58:19 +0530

    refactor: Enhance documentation, improve error handling
    - Improve/Add documentation for more clarity
    - Refactor code according to rust standards
    - Improve error handling
    
    Closes #1

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,25 @@
-// Not for now - Run(for cli): `cargo run --release -- -p "What do you think about Rust lang?" -m ./open_llama_3b-f16.bin`
-
 // Example:
 // http://127.0.0.1:8000/
 // http://127.0.0.1:8000/chat
 // http://127.0.0.1:8000/chat/this is my prompt
 // http://127.0.0.1:8000//chat?prompt=this is my prompt
 
-// Module for LLM models
+/// Module for LLM models
 pub mod chat;
-// Module for axum server
+/// Module for axum server
 pub mod server;
 
 use clap::Parser;
 use server::start_server;
 
+// TODO: (using CLI) `cargo run --release -- -m ./open_llama_3b-f16.bin`
+
+/// CLI argument for model path
 #[derive(Parser, Debug)]
 struct Args {
-    #[clap(short, long)]
+    #[clap(short = 'p', long = "path")]
     model_path: std::path::PathBuf,
-
-    #[clap(short, long)]
-    prompt: String,
 }
-
-// #[derive(Debug, thiserror::Error)]
-// enum Error {
-//     #[error("Unable to start server")]
-//     UnableToStartServer,
-// }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,7 @@
+use std::net::SocketAddr;
+
 use crate::chat::Prompt;
+
 use axum::{
     extract::{Path, Query},
     response::{Html, IntoResponse},
@@ -6,61 +9,76 @@ use axum::{
     Json, Router,
 };
 use serde_json::json;
-use std::net::SocketAddr;
 
-// Handles the root("/")
-async fn handler_root() -> impl IntoResponse {
-    colour::blue_ln!(">> HANDLER - Root");
-    Html("<h1>Welcome to Chatbot</h1>
-<p><strong>Suggestion</strong>: To initiate a chat, add <code>/chat?prompt=this is prompt</code> or <code>/chat/this is prompt</code> to the path of this site.</p>")
+/// Custom server errors
+#[derive(Debug, thiserror::Error)]
+enum ServerError {
+    #[error("Unable to start server due to: {0}")]
+    UnableToStartServer(String),
 }
 
-// Handles the "/chat?prompt='..'"
-async fn handler_chat_query(Query(prompt): Query<Prompt>) -> impl IntoResponse {
-    colour::blue_ln!(">> HANDLER - Handling /chat - {:?}", &prompt);
-
-    let llm_response = match prompt.prompt() {
-        Some(_) => prompt.infer().unwrap(),
-        None => {
-            return Json(
-                json!({"Suggestion":"To initiate a chat, add \"/chat?prompt=my prompt\" or \"/chat/my prompt\""}),
-            );
-            // return Html("Do /chat?prompt=my prompt");
-        }
-    };
-
-    Json(json!({"prompt": prompt.prompt(), "response": llm_response.response()}))
-}
-
-// Handles the "/chat/my prompt.."
-async fn handler_chat_path(Path(prompt): Path<Prompt>) -> impl IntoResponse {
-    colour::blue_ln!(">> HANDLER - Handling /chat - {:?}", &prompt);
-
-    let llm_response = match prompt.prompt() {
-        Some(_) => prompt.infer().unwrap(),
-        None => {
-            return Json(json!({"Suggestion":"To chat do \"/chat?prompt=my prompt\""}));
-            // return Html("Do /chat?prompt=my prompt");
-        }
-    };
-
-    Json(json!({"prompt": prompt.prompt(), "response": llm_response.response()}))
-}
-
+/// Starts the server and handles incoming requests.
 pub async fn start_server() -> anyhow::Result<()> {
     let router: Router = Router::new()
+        // Defines a route for the root URL ("/")
         .route("/", get(handler_root))
+        // Defines a route for the "/chat" URL
         .route("/chat", get(handler_chat_query))
+        // Defines a route with a dynamic segment "/chat/:prompt"
         .route("/chat/:prompt", get(handler_chat_path));
 
-    // Making a local host
+    // Representing the IP address and port for the server.
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
 
     // ---------- Start the server ---------------
     colour::green_ln!(">> Listening on {addr}\n");
     axum::Server::bind(&addr)
         .serve(router.into_make_service())
-        .await?;
+        .await
+        .map_err(|e| ServerError::UnableToStartServer(e.to_string()))?;
 
     Ok(())
+}
+
+/// Handles the root("/")
+async fn handler_root() -> impl IntoResponse {
+    colour::blue_ln!(">> HANDLER - Root");
+    Html("<h1>Welcome to Chatbot</h1>
+<p><strong>Suggestion</strong>: To initiate a chat, add <code>/chat?prompt=this is prompt</code> or <code>/chat/this is prompt</code> to the path of this site.</p>")
+}
+
+/// Handles the "/chat?prompt='..'"
+async fn handler_chat_query(Query(prompt): Query<Prompt>) -> impl IntoResponse {
+    colour::blue_ln!(">> HANDLER - Handling /chat - {:?}", &prompt);
+
+    // Check for prompt
+    match prompt.prompt() {
+        // Create LLM response
+        Some(_) => {
+            let llm_response = prompt.infer().unwrap();
+            Json(json!({"prompt": prompt.prompt(), "response": llm_response.response()}))
+        }
+        // Give instruction of "how to chat?"
+        None => Json(
+            json!({"Suggestion":"To initiate a chat, add \"/chat?prompt=my prompt\" or \"/chat/my prompt\""}),
+        ),
+    }
+}
+
+/// Handles the "/chat/my prompt.."
+async fn handler_chat_path(Path(prompt): Path<Prompt>) -> impl IntoResponse {
+    colour::blue_ln!(">> HANDLER - Handling /chat - {:?}", &prompt);
+
+    // Check for prompt
+    match prompt.prompt() {
+        // Create LLM response
+        Some(_) => {
+            let llm_response = prompt.infer().expect("Failed to perform inference");
+            Json(json!({"prompt": prompt.prompt(), "response": llm_response.response()}))
+        }
+        // Give instruction of "how to chat?"
+        None => Json(
+            json!({"Suggestion":"To initiate a chat, add \"/chat?prompt=my prompt\" or \"/chat/my prompt\""}),
+        ),
+    }
 }

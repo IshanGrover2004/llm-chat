8129cef4f1967b2543a55459edda5014f061e69c
Ishan Grover <123376573+IshanGrover2004@users.noreply.github.com>
2023-11-10 21:13:23 +0530

    fix: Unusual behaviour of setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ download_url="https://huggingface.co/rustformers/open-llama-ggml/resolve/main/op
 
 download_path="./assets/open_llama_3b-f16.bin"
 
-mkdir -p $download_path
+mkdir -p ./assets
 
 wget -O $download_path $download_url
 
